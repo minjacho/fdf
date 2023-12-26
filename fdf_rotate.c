82c823a0d150eb39d9133a5bf26a553354b526dc
Minjae Choi <minjacho@c2r4s2.42seoul.kr>
2023-12-26 20:09:21 +0900

    rotate x and y modified

--- fdf_rotate.c
+++ fdf_rotate.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   rotate.c                                           :+:      :+:    :+:   */
+/*   fdf_rotate.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: minjacho <minjacho@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/22 19:37:57 by minjacho          #+#    #+#             */
-/*   Updated: 2023/12/26 17:24:12 by minjacho         ###   ########.fr       */
+/*   Updated: 2023/12/26 20:03:05 by minjacho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,9 +18,9 @@ void	rotate_x_axis(t_point *point, double theta)
 	int	new_y;
 	int	new_z;
 
-	new_x = point->x * cos(theta) - point->z * sin(theta);
-	new_y = point->y;
-	new_z = point->x * sin(theta) + point->z * cos(theta);
+	new_x = point->x;
+	new_y = point->y * cos(theta) + point->z * sin(theta);
+	new_z = -point->y * sin(theta) + point->z * cos(theta);
 	point->x = new_x;
 	point->y = new_y;
 	point->z = new_z;
@@ -32,9 +32,9 @@ void	rotate_y_axis(t_point *point, double theta)
 	int	new_y;
 	int	new_z;
 
-	new_x = point->x;
-	new_y = point->y * cos(theta) + point->z * sin(theta);
-	new_z = -point->y * sin(theta) + point->z * cos(theta);
+	new_x = point->x * cos(theta) - point->z * sin(theta);
+	new_y = point->y;
+	new_z = point->x * sin(theta) + point->z * cos(theta);
 	point->x = new_x;
 	point->y = new_y;
 	point->z = new_z;
